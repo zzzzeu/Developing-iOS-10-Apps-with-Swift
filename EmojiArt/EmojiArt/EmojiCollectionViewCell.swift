ae7b626d58f88c19e27235fcde5d92f8217f9c2c
zzzzeu <zouanei@outlook.com>
2019-03-11 23:19:55 +0800

    lecture 12

---
+++ EmojiArt/EmojiArt/EmojiCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  EmojiCollectionViewCell.swift
+//  EmojiArt
+//
+//  Created by 总受 on 2019/3/11.
+//  Copyright © 2019 com.zeu. All rights reserved.
+//
+
+import UIKit
+
+class EmojiCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var label: UILabel!
+    
+    
+}
