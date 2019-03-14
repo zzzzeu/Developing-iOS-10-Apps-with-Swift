6fd47f691a72dfafcc58f7c0777e26d04a8f5880
zzzzeu <zouanei@outlook.com>
2019-03-14 17:25:16 +0800

    lecture 13

---
+++ EmojiArt/EmojiArt/TextFieldCollectionViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  TextFieldCollectionViewCell.swift
+//  EmojiArt
+//
+//  Created by 总受 on 2019/3/13.
+//  Copyright © 2019 com.zeu. All rights reserved.
+//
+
+import UIKit
+
+class TextFieldCollectionViewCell: UICollectionViewCell, UITextFieldDelegate {
+    
+    @IBOutlet weak var textField: UITextField! {
+        didSet {
+            textField.delegate = self
+        }
+    }
+    
+    var resignationHandler: (() -> Void)?
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        resignationHandler?()
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+}
